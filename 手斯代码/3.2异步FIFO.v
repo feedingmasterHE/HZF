51c2ed12d3bd871e022fbd24af012168df868bec
uqdxz <uqdxz@itiv-pool-217.itiv.kit.edu>
2022-06-24 11:17:14 +0200

    update asynchron FIFO

--- 手斯代码/3.2异步FIFO.v
+++ 手斯代码/3.2异步FIFO.v
@@ -34,13 +34,76 @@ module Asychron_FIFO
     input re,
     output full,
     output empty,
-    output[wsize-1:0] rdata
+    output reg [wsize-1:0] rdata
     );
     
     reg[wsize-1:0] ram[dsize-1:0];
-    reg[wsize-1:0] waddr, raddr;
-    reg[wsize:0] wbin, rbin, wbin_next, rbin_next;
-    reg[wsize:0] wgrey, 
+    wire[wsize-1:0] waddr, raddr;
+    reg[wsize:0] waddr_bin, raddr_bin;
+    wire[wsize:0] waddr_grey;
+    reg[wsize:0] waddr_grey_d1, waddr_grey_d2;
+    wire[wsize:0] raddr_grey;
+    reg[wsize:0] raddr_grey_d1, raddr_grey_d2;
     
-    //https://www.cnblogs.com/ylsm-kb/p/9068449.html
+    //生成二进制写指针, 通过将MSB-1到0位地址赋给地址来写入数据
+    always@(posedge clk_wr or negedge rst) begin
+        if(!rst)
+            waddr_bin <= 'h0;
+        else if(we && !full)
+            waddr_bin <= waddr_bin + 1;
+        else
+            waddr_bin <= waddr_bin;
+     end
+     
+     always@(posedge clk_wr) begin
+        if(we && !full)
+            ram[waddr] <= wdata;
+        else
+            ram[waddr] <= ram[waddr];
+     end
+     
+     assign waddr = waddr_bin[wsize-1:0];
+     
+     //生成二进制读指针,通过将MSB-1到0位地址赋给读地址来读出数据
+     always@(posedge clk_rd or negedge rst) begin
+        if(!rst)
+            raddr_bin <= 'h0;
+        else if(!empty && re)
+            raddr_bin <= raddr_bin + 1;
+        else
+            raddr_bin <= raddr_bin;
+     end
+     
+     always@(posedge clk_rd) begin
+        if(!empty && re)
+            rdata <= ram[raddr];
+        else
+            rdata <= 'h0;
+     end
+     
+     assign raddr = raddr_bin[wsize-1:0];
+     
+     //将二进制地址转换到格雷码并同步到读写时钟域来做比较产生空满信号
+     assign waddr_grey = (waddr_bin >> 1) ^ waddr_bin;
+     assign raddr_grey = (raddr_bin >> 1) ^ raddr_bin;
+     
+     always@(posedge clk_rd) begin
+        waddr_grey_d1 <= waddr_grey;
+        waddr_grey_d2 <= waddr_grey_d1;
+     end
+     
+     always@(posedge clk_wr) begin
+        raddr_grey_d1 <= raddr_grey;
+        raddr_grey_d2 <= raddr_grey_d1;
+     end
+     //高两位不同，其余位都相同
+     assign full = (waddr_grey == {~raddr_grey_d2[wsize : wsize-1], raddr_grey_d2[wsize-2:0]});
+     //所有位都相同
+     assign empty = (raddr_grey == waddr_grey_d2);
+ 
+        
+          
+     
+     
+   
 endmodule
